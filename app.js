4662fac695645e7e714c2b2a6e10c2513fa84d17
MICHEL VICTOR <michelvictor.doc@gmail.com>
2020-07-20 14:53:14 -0300

    add .env

--- app.js
+++ app.js
@@ -18,10 +18,9 @@ app.get('/happybirthday', (req, res) =>{
         ${String(name).toUpperCase()}! </h1>
         <p> you are ${age} years old now. </p>
         <img src="https://media.giphy.com/media/SRO0ZwmImic0/giphy.gif" alt="cat">
-
     `)
 })
 
-app.listen(3333, () =>{
+app.listen(process.env.PORT || 3333, () =>{
     console.log('server it running')
 })
